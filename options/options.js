6149b943bff15f0f19960ee01a5eebe3ad980bf1
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 00:20:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-night-dev-78527",
+  url: "https://weathered-night-dev-78527.botics.co",
+  api: "https://weathered-night-dev-78527.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
